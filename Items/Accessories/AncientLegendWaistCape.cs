14f33af362a4368db31c55dd3945f92602237868
Straws <nguyen.trungt@yahoo.com>
2020-02-02 03:31:41 -0600

    Gaara's Accessories Added:      - Large Gourd           - Requires Rebalancing      - Ancient Legend Waist Cape           - Requires Rebalancing

---
+++ Items/Accessories/AncientLegendWaistCape.cs
@@ -0,0 +1,46 @@
+﻿﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NaruMod.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Waist)]
+    public class AncientLegendWaistCape : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("A ancient garment made to imitate Gaara's bottom half of his outfit.");
+            DisplayName.SetDefault("Ancient Legend Waistcape");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 24;
+            item.height = 28;
+            item.value = 150000;
+            item.rare = 5;
+            item.accessory = true;
+            item.defense = 0;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            {
+//                player.GetModPlayer<MyPlayer>(mod).KiDamage *= 1.06f;
+//                player.GetModPlayer<MyPlayer>(mod).kiDrainMulti *= 0.86f;
+//                player.GetModPlayer<MyPlayer>(mod).kiMax2 -= 250;
+//                player.GetModPlayer<MyPlayer>(mod).legendWaistcape = true;
+            }
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.HardenedSand, 25);
+            recipe.AddIngredient(ItemID.Silk, 12);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Items/Accessories/LargeGourd.cs
@@ -0,0 +1,49 @@
+﻿﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NaruMod.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Back)]
+    public class LargeGourd : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("'An overgrown Gourd filled with Chakra. Oddly filled with living sand that seems to really like you.'"
+               + "\n  8  increased magic crit"
+               + "\n 20% increased mana regen"
+               + "\n 10% decreased magic damage");
+            DisplayName.SetDefault("Large Sand-Filled Gourd");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 46;
+            item.height = 30;
+            item.value = 70000;
+            item.rare = 4;
+            item.accessory = true;
+            item.defense = 5;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            {
+                player.manaRegen *= 2;
+                player.magicCrit += 8;
+                player.magicDamage -= 0.1F;
+            }
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.SandBlock, 50);
+            recipe.AddIngredient(ItemID.Silk, 20);
+            recipe.AddIngredient(ItemID.ClayBlock, 10);
+            recipe.AddIngredient(ItemID.ManaCrystal, 5);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
